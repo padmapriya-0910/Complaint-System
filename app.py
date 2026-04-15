b1d60d4f1e4d3c28d71431745937a6455ee1ef85
Padma Priya <your-github-email@example.com>
2026-04-15 14:11:12 +0530

    first commit

--- app.py
+++ app.py
@@ -177,4 +177,4 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000, debug=False)
